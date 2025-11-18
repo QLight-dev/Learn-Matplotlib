2d4e4aa3d70eb2759dbaa2e31121905c46ad19e0
Muhammed Uzair <nayazmuhammed094@gmail.com>
2025-11-18 05:36:38 +0000

    Completed 3 - Matplotlib markers https://www.w3schools.com/python/matplotlib_markers.asp Learnt how to create a marker Learnt how to use fmt Learnt how to change marker size Learnt hwo to change marker color

---
+++ c3.py
@@ -0,0 +1,22 @@
+import matplotib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt 
+import numpy as np 
+
+# Markers
+ypoints = np.array([1, 4, 2, 7, 5, 9, 5])
+plt.plot(ypoints, marker= 'o')
+plt.show()
+
+# Format Strings
+# Marker:line:color
+
+plt.plot(ypoints, 'o:g')
+plt.show()
+
+# Marker Size
+plt.plot(ypoints, marker = '*', ms = '30')
+plt.show()
+
+# Marker Color
+plt.plot(ypoints, marker = 'o', ms = 10, mec = 'g')
